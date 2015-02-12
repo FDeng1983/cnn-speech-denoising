c72f0cbc4c4906d8c49560dcd24b10bdb72d18da
Victor Zhong <victor@victorzhong.com>
2015-02-11 16:15:54 -0800

    pushing downloader script

---
+++ dataset/download.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+wget --user=anonymous ftp://ftp.dcs.shef.ac.uk/share/spandh/chime_challenge/grid/train_clean.tgz
+#wget --user=anonymous ftp://ftp.dcs.shef.ac.uk/share/spandh/chime_challenge/grid/train_reverberated.tgz
+wget --user=anonymous ftp://ftp.dcs.shef.ac.uk/share/spandh/chime_challenge/grid/train_isolated.tgz
+#wget --user=anonymous ftp://ftp.dcs.shef.ac.uk/share/spandh/chime_challenge/grid/train_embedded1.tgz
+#wget --user=anonymous ftp://ftp.dcs.shef.ac.uk/share/spandh/chime_challenge/grid/train_embedded2.tgz
+
+#wget --user=anonymous ftp://ftp.dcs.shef.ac.uk/share/spandh/chime_challenge/grid/devel_isolated.tgz
+#wget --user=anonymous ftp://ftp.dcs.shef.ac.uk/share/spandh/chime_challenge/grid/devel_embedded.tgz
+#wget --user=anonymous ftp://ftp.dcs.shef.ac.uk/share/spandh/chime_challenge/grid/devel_reverberated.tgz
+
+#wget --user=anonymous ftp://ftp.dcs.shef.ac.uk/share/spandh/chime_challenge/grid/test_isolated.tgz
+#wget --user=anonymous ftp://ftp.dcs.shef.ac.uk/share/spandh/chime_challenge/grid/test_embedded.tgz
+
