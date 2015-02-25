d0f874180629cedc26b5a45962843e1e54afa04c
Mike Kayser <mkayser@julie1.stanford.edu>
2015-02-25 14:15:27 -0800

    Merge

--- dataset/utils/generate_all_mfcc_and_spec.py
+++ dataset/utils/generate_all_mfcc_and_spec.py
@@ -2,13 +2,13 @@ from subprocess import call
 import os
 
 dirs = [
-    '/home/mkayser/school/classes/2014_2015_win/cs231n/proj/chime13/data/aasp-chime-grid/train/clean',
-    '/home/mkayser/school/classes/2014_2015_win/cs231n/proj/chime13/data/aasp-chime-grid/train/isolated',
-    '/home/mkayser/school/classes/2014_2015_win/cs231n/proj/chime13/data/aasp-chime-grid/devel/isolated',
-    '/home/mkayser/school/classes/2014_2015_win/cs231n/proj/chime13/data/aasp-chime-grid/test/isolated']
+    '/juicer/scr81/scr/mkayser/conv/cnn-speech-denoising/dataset/aasp-chime-grid/train/clean',
+    '/juicer/scr81/scr/mkayser/conv/cnn-speech-denoising/dataset/aasp-chime-grid/train/isolated',
+    '/juicer/scr81/scr/mkayser/conv/cnn-speech-denoising/dataset/aasp-chime-grid/devel/isolated',
+    '/juicer/scr81/scr/mkayser/conv/cnn-speech-denoising/dataset/aasp-chime-grid/test/isolated']
         
 for mydir in dirs:
     for mysubdir in os.listdir(mydir):
         path = '{}/{}'.format(mydir,mysubdir)
-        command = ['octave', '--eval', '"iter_calc_mfcc {}"'.format(path)]
+        command = ['matlab', '-r', '"iter_calc_mfcc {}"'.format(path)]
         print " ".join(command)
