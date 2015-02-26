11dc99c77779ce85dd6d39a4e34830e75f90a4a7
Victor Zhong <victor@victorzhong.com>
2015-02-26 10:21:04 -0800

    adding skinny filters

--- models/model0/train.sh
+++ models/model0/train.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env sh
 
+if [ $# -eq 0 ]
+then
+	NET="auto_net"
+else
+	NET="$1"
+fi
+
+
 ./build/tools/caffe train \
-  --solver=project/cnn-speech-denoising/models/model0/auto_net_solver.prototxt
+  --solver=project/cnn-speech-denoising/models/model0/${NET}_solver.prototxt
