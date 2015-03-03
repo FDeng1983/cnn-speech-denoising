cb693aa22a510962e5fdca4ed4b06eab3a35ba2b
Victor Zhong <victor@victorzhong.com>
2015-03-02 23:54:00 -0800

    pushing updated api, best sampling settings

--- patch_sampler.py
+++ patch_sampler.py
@@ -3,10 +3,12 @@
 Usage:
   patch_sampler.py  <root_dir>
                     [--samples_per_spectrogram=<samples_per_spectrogram>]
-                    [--max_examples_per_file=<max_examples_per_file>]
                     [--verbose]
-                    [--x_len=<100>]
-                    [--scale=<1e-5>]
+                    [--x_len=<20>]
+                    [--max_mfcc=inf]
+                    [--min_mfcc=-inf]
+                    [--scale_mfcc=1]
+                    [--normalize_spec]
 """
 
 import os
@@ -86,25 +88,18 @@ if __name__ == '__main__':
     from docopt import docopt
     args = docopt(__doc__)
 
-    args['--x_len'] = int(args['--x_len']) if args['--x_len'] else 20
-    args['--scale'] = float(args['--scale']) if args['--scale'] else 1
-    args['--verbose'] = True if args['--verbose'] else False
+    def default(key, type_, val):
+        args[key] = type_(args[key]) if args[key] else val
+
+    default('--x_len', int, 20)
+    default('--samples_per_spectrogram', int, 5)
+    default('--max_mfcc', float, np.inf)
+    default('--min_mfcc', float, -np.inf)
+    default('--scale_mfcc', float, 1)
 
     from pprint import pprint
     print 'User arguments:'
     pprint(args)
-
-    # the number of examples to write to the output hdf5 file before switching to a new one
-    if args['--max_examples_per_file']:
-        max_examples_per_file = int(args['--max_examples_per_file'])
-    else:
-        max_examples_per_file = 1000
-
-    # how many times to sample from a given spectrogram
-    if args['--samples_per_spectrogram']:
-        samples_per_spectrogram = int(args['--samples_per_spectrogram'])
-    else:
-        samples_per_spectrogram = 5
 
     # where are the spectrograms
     root = args['<root_dir>']
@@ -116,50 +111,32 @@ if __name__ == '__main__':
     def get_fname(filenum):
         return os.path.join(out_dir, 'file.' + str(filenum) + '.h5')
 
-    sampler = PatchSampler(spec_dir, mfcc_dir, samples_per_spectrogram, 
+    sampler = PatchSampler(spec_dir, mfcc_dir, args['--samples_per_spectrogram'],
         verbose=args['--verbose'],
         x_len=args['--x_len'])
 
-    patches_sampled = 0; filenum = 0; total_patches_seen = 0
-    spec = None; mfcc = None; mfcc_mean = None; spec_mean = None
-
-    max_seen = 0
-    min_seen = np.inf
-
+    spec = []; mfcc = []
     for spec_patch, mfcc_patch in sampler:
-        patches_sampled += 1
-
-        max_seen = np.max([spec_patch.max(), mfcc_patch.max(), max_seen])
-        min_seen = np.min([spec_patch.min(), mfcc_patch.min(), min_seen])
-
-        # scale if requested
-        spec_patch *= args['--scale']
-        mfcc_patch *= args['--scale']
-
-        # update the running means
-        mfcc_mean = mfcc_patch.copy() if mfcc_mean is None else (mfcc_mean * total_patches_seen + mfcc_patch) / float(total_patches_seen+1)
-        spec_mean = spec_patch.copy() if spec_mean is None else (spec_mean * total_patches_seen + spec_patch) / float(total_patches_seen+1)
 
         # concatenate the patches to buffer the write
-        spec = spec_patch if spec is None else np.concatenate((spec, spec_patch), axis=0)
-        mfcc = mfcc_patch if mfcc is None else np.concatenate((mfcc, mfcc_patch), axis=0)
+        spec += [spec_patch]
+        mfcc += [mfcc_patch]
 
-        if patches_sampled == max_examples_per_file:
-            print 'writing out files', filenum
-            # write a new file
-            write_out(spec.astype('float32'), mfcc.astype('float32'), get_fname(filenum))
-            filenum += 1
-            # empty buffers
-            spec = None; mfcc = None; patches_sampled = 0
+    # Concatenate
+    spec = np.concatenate(spec, axis=0)
+    mfcc = np.concatenate(mfcc, axis=0)
 
-    print 'maximum magnitude seen:', max_seen
-    print 'minimum magnitude seen:', min_seen
+    mfcc.clip(args['--min_mfcc'], args['--max_mfcc'])
+    mfcc *= args['--scale_mfcc']
+
+    if args['--normalize_spec']:
+        spec = (spec - spec.mean(axis=0)) / spec.std(axis=0)
+
+    print 'writing out files'
+    # write a new file
+    write_out(spec.astype('float32'), mfcc.astype('float32'), get_fname(0))
 
     # write out book keeping files
     with open(os.path.join(out_dir, 'filelist.txt'), 'wb') as f:
-        for i in xrange(filenum):
-            f.write('project/cnn-speech-denoising/' + get_fname(i) + '\n')
+        f.write('project/cnn-speech-denoising/' + get_fname(0) + '\n')
 
-    # write out accumulated means
-    with open(os.path.join(out_dir, 'means.pkl'), 'wb') as f:
-        pkl.dump({'mfcc': mfcc_mean, 'spec': spec_mean}, f)
