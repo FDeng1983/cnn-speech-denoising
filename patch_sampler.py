072578b5cf73ce71325b1c9a84d88c2a50e843b1
Victor Zhong <victor@victorzhong.com>
2015-03-02 17:30:34 -0800

    sample mfccs instead

--- patch_sampler.py
+++ patch_sampler.py
@@ -13,54 +13,62 @@ import h5py
 import cPickle as pkl
 
 
-class SpectrogramPair(object):
+class SpectrogramMFCC(object):
 
-    def __init__(self, noisy_mat, clean_mat, num_samples):
-        with h5py.File(noisy_mat, 'r') as f:
-            self.noisy_mat = f['ps/value'].value
-        with h5py.File(clean_mat, 'r') as f:
-            self.clean_mat = f['ps/value'].value
+    def __init__(self, spec_mat, mfcc_mat, num_samples):
+        try:
+            with h5py.File(spec_mat, 'r') as f:
+                self.spec_mat = f['ps/value'].value
+        except Exception as e:
+            print 'Error in file', spec_mat
+            raise e
+        try:
+            with h5py.File(mfcc_mat, 'r') as f:
+                self.mfcc_mat = f['mfc/value'].value
+        except Exception as e:
+            print 'Error in file', mfcc_mat
+            raise e
         self.num_samples = num_samples
 
-    def sample_patch(self, y_len, x_len):
+    def sample_patch(self, x_len):
 
-        y_max, x_max = self.clean_mat.shape
+        x_max, y_max = self.spec_mat.shape
 
         for i in xrange(self.num_samples):
-            y_start = np.random.randint(low=0, high=y_max-y_len)
             x_start = np.random.randint(low=0, high=x_max-x_len)
 
-            clean_patch = self.clean_mat[y_start:y_start+y_len, x_start:x_start+x_len]
-            noisy_patch = self.noisy_mat[y_start:y_start+y_len, x_start:x_start+x_len]
+            mfcc_patch = self.mfcc_mat[x_start:x_start+x_len, :]
+            spec_patch = self.spec_mat[x_start:x_start+x_len, :]
 
-            yield noisy_patch.reshape(1, 1, y_len, x_len), clean_patch.reshape(1, 1, y_len, x_len)
+            assert mfcc_patch.shape == (spec_patch.shape[0], 39)
+
+            yield spec_patch.reshape(1, 1, -1, x_len), mfcc_patch.reshape(1, 1, -1, x_len)
 
 
 class PatchSampler(object):
 
-    def __init__(self, clean_dir, noisy_dir, samples_per_spectrogram=10, x_len=100, y_len=100, verbose=False):
-        self.clean_dir = clean_dir
-        self.noisy_dir = noisy_dir
+    def __init__(self, spec_dir, mfcc_dir, samples_per_spectrogram=10, x_len=100, verbose=False):
+        self.spec_dir = spec_dir
+        self.mfcc_dir = mfcc_dir
         self.n = samples_per_spectrogram
         self.x_len = x_len
-        self.y_len = y_len
         self.verbose = verbose
 
     def __iter__(self):
-        for root, dirs, files in os.walk(self.clean_dir, topdown=False):
+        for root, dirs, files in os.walk(self.mfcc_dir, topdown=False):
             if self.verbose:
                 print 'sampling from folder', root
 
             for name in files:
                 if not name.endswith('.hdf5'): continue
 
-                clean = os.path.join(root, name)
-                noisy = clean.replace(self.clean_dir, self.noisy_dir)
-                assert os.path.isfile(clean)
-                assert os.path.isfile(noisy)
+                mfcc = os.path.join(root, name)
+                spec = mfcc.replace(self.mfcc_dir, self.spec_dir).replace('wav.mfcc', 'wav.spec')
+                assert os.path.isfile(mfcc), "mfcc file %s does not exist" % mfcc
+                assert os.path.isfile(spec), "spec file %s does not exist" % spec
 
-                pair = SpectrogramPair(noisy, clean, self.n)
-                for idx, patches in enumerate(pair.sample_patch(x_len=self.x_len, y_len=self.y_len)):
+                pair = SpectrogramMFCC(spec, mfcc, self.n)
+                for idx, patches in enumerate(pair.sample_patch(x_len=self.x_len)):
                     yield patches
 
 
@@ -94,36 +102,36 @@ if __name__ == '__main__':
 
     # where are the spectrograms
     root = args['<root_dir>']
-    clean_dir = os.path.join(root, 'clean')
-    noisy_dir = os.path.join(root, 'noisy')
+    spec_dir = os.path.join(root, 'spec')
+    mfcc_dir = os.path.join(root, 'mfcc')
     out_dir = os.path.join(root, 'sampled')
     if not os.path.isdir(out_dir): os.makedirs(out_dir)
 
     def get_fname(filenum):
         return os.path.join(out_dir, 'file.' + str(filenum) + '.h5')
 
-    sampler = PatchSampler(clean_dir, noisy_dir, samples_per_spectrogram, verbose=True if args['--verbose'] else False)
+    sampler = PatchSampler(spec_dir, mfcc_dir, samples_per_spectrogram, verbose=True if args['--verbose'] else False)
 
     patches_sampled = 0; filenum = 0; total_patches_seen = 0
-    noisy = None; clean = None; clean_mean = None; noisy_mean = None
-    for noisy_patch, clean_patch in sampler:
+    spec = None; mfcc = None; mfcc_mean = None; spec_mean = None
+    for spec_patch, mfcc_patch in sampler:
         patches_sampled += 1
 
         # update the running means
-        clean_mean = clean_patch.copy() if clean_mean is None else (clean_mean * total_patches_seen + clean_patch) / float(total_patches_seen+1)
-        noisy_mean = noisy_patch.copy() if noisy_mean is None else (noisy_mean * total_patches_seen + noisy_patch) / float(total_patches_seen+1)
+        mfcc_mean = mfcc_patch.copy() if mfcc_mean is None else (mfcc_mean * total_patches_seen + mfcc_patch) / float(total_patches_seen+1)
+        spec_mean = spec_patch.copy() if spec_mean is None else (spec_mean * total_patches_seen + spec_patch) / float(total_patches_seen+1)
 
         # concatenate the patches to buffer the write
-        noisy = noisy_patch if noisy is None else np.concatenate((noisy, noisy_patch), axis=0)
-        clean = clean_patch if clean is None else np.concatenate((clean, clean_patch), axis=0)
+        spec = spec_patch if spec is None else np.concatenate((spec, spec_patch), axis=0)
+        mfcc = mfcc_patch if mfcc is None else np.concatenate((mfcc, mfcc_patch), axis=0)
 
         if patches_sampled == max_examples_per_file:
             print 'writing out files', filenum
             # write a new file
-            write_out(noisy.astype('float32'), clean.astype('float32'), get_fname(filenum))
+            write_out(spec.astype('float32'), mfcc.astype('float32'), get_fname(filenum))
             filenum += 1
             # empty buffers
-            noisy = None; clean = None; patches_sampled = 0
+            spec = None; mfcc = None; patches_sampled = 0
 
     # write out book keeping files
     with open(os.path.join(out_dir, 'filelist.txt'), 'wb') as f:
@@ -132,4 +140,4 @@ if __name__ == '__main__':
 
     # write out accumulated means
     with open(os.path.join(out_dir, 'means.pkl'), 'wb') as f:
-        pkl.dump({'clean': clean_mean, 'noisy': noisy_mean}, f)
+        pkl.dump({'mfcc': mfcc_mean, 'spec': spec_mean}, f)
